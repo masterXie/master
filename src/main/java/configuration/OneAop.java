dc8e074b7a10a4a5e97dfd171ba44c5d06495f0f
masterXie <xielei277@163.com>
2017-02-14 15:57:42 +0800

    Update OneAop.java

--- src/main/java/configuration/OneAop.java
+++ src/main/java/configuration/OneAop.java
@@ -1,14 +1,42 @@
-package configuration;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+package master.configuration;
 
-@Configuration
-@EnableAspectJAutoProxy
-@ComponentScan
-public class ConcertConfig {
-	public OneAop oneAop(){
-		return new OneAop();
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(0)
+public class OneAop {
+	
+	@Pointcut("execution(public * master.serviceImpl.HomeServiceImpl.*(..))")
+	public void pointCut(){
+		System.out.println("切点");
 	}
+	
+	@Before("pointCut()")
+	public void before(){
+		System.out.println("前置通知");
+	}
+	
+	@AfterReturning("pointCut()")  
+    public void doAfter(){  
+        System.out.println("后置通知");  
+    }  
+      
+    @After("pointCut()")  
+    public void after(){  
+        System.out.println("最终通知");  
+    }  
+      
+    @AfterThrowing("pointCut()")  
+    public void doAfterThrow(){  
+        System.out.println("例外通知");  
+    }
 }
